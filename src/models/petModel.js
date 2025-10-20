e0a899dbcea24cb68c32c9d4713be1e547250b82
Ana Julia <ana.j.ferreira15@aluno.senai.br>
2025-10-20 11:43:08 -0300

    Feat: Adiciona Get All e Get By Id na atividade

---
+++ src/models/petModel.js
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+//Crio a variavél findAll e já exporto
+export const findAll = async () => {
+
+    return await prisma.pet.findMany({
+    orderBy: { nome: 'asc' }
+    });
+}
+
+//Crio a variavel findById e já exporto
+export const findById = async (id) => {
+
+    return await prisma.pet.findUnique({
+        where: { id: Number(id) }
+    })
+}
